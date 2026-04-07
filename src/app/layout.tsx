cae6cee6bcd9bbd93e4d5a9e7402fd9e1b9c0860
CristopheChen <cristophe.chen@gmail.com>
2026-04-07 01:57:37 -0400

    add speed analysis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { MusicProvider } from "../lib/MusicContext";
 import "./globals.css";
@@ -100,6 +101,7 @@ export default function RootLayout({
           {children}
         </MusicProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
